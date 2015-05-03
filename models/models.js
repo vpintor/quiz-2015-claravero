6157b68c82fa421fb13165f13fdb976f1313633a
Clara Jimenez <clarajimenezrecio@gmail.com>
2015-05-03 13:27:02 +0200

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,27 +1,55 @@
 var path = require('path');
 
-// Cargar Modelo ORM
+
+//Postgress DATABAS_URL = postgress://user:passwd@host:port/database
+//Sqlite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user  = (url[2] || null);
+var pwd  = (url[3] || null);
+var protocol  = (url[1] || null);
+
+//******************************************************
+//cambio el dialecto manualmente
+var dialect = "sqlite";
+//*******************************************************
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+//CARGAR modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null, 
-                       {dialect: "sqlite", storage: "quiz.sqlite"}
-                    );
+//USAR BBDD SQlite o  Postgres
+var sequelize = new Sequelize(DB_name,user,pwd,
+                        {
+//*****************************************************
+//llamo al dialecto puesto manualmente
+dialect: dialect,
+//*****************************************************
+                        protocol: protocol,
+                        port: port,
+                        host: host,
+                        storage: storage,
+                        omitNull: true
+                    }
+                );
 
-// Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+//Importar la definicion de la tabla quiz en quiz.js
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Quiz = Quiz; //exportar definicion de la tabla Quiz
 
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-  // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count){
-    if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia',
-                    respuesta: 'Roma'
-                 })
-      .success(function(){console.log('Base de datos inicializada')});
-    };
-  });
+//sequelize.sync() crea e inicializa la tabla de preguntas en DB
+sequelize.sync().then(function() {
+    //then(..) ejecuta el manejador una vez creada la tabla
+    Quiz.count().then(function(count){
+        if(count === 0) { //la tabla se inicializa solo si esta vacia
+            Quiz.create({ pregunta:'Capital de Italia',
+                            respuesta: 'Roma'
+                        })
+            .then( function(){console.log('Base de datos inicializa')});
+        };
+    });
 });
