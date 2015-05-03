986b1c9aeea2538cbde463db3ee3dfdf3b713b6e
Clara Jimenez <clarajimenezrecio@gmail.com>
2015-05-03 19:30:47 +0200

    Autorización

--- routes/author.js
+++ routes/author.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('author', { title: 'Author' });
+  res.render('author', { title: 'Author', errors: []});
 });
 
 module.exports = router;
