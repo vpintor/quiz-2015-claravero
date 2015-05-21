b5541278ed2bf98956dc5badc3d547e940632709
Veronica Pintor <v.pintor@alumnos.upm.es>
2015-05-21 23:52:36 +0200

    Registro y gestión de usuarios

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,18 @@
 var models = require('../models/models.js');
 
+//Autoload :userId
+exports.load = function(req, res, next, userId) {
+  models.User.find({where: { id: Number(userId)}})
+  .then(function(user){
+    if(user) {
+      req.user = user;
+      next();
+    }
+    else{next(new Error ('No existe userId= ' + userId))}
+  }
+  ).catch(function(error){next(error)});
+};
+
 // Comprueba si el usuario esta registrado en users
 // Si autenticación falla o hay errores se ejecuta callback(error).
 exports.autenticar = function(login, password, callback) {
@@ -18,4 +31,69 @@ exports.autenticar = function(login, password, callback) {
       }
       else { callback(new Error('No existe el usuario: ' + login))}
     }).catch(function(error){callback(error)});
+};
+
+//GET /user/:id/edit
+exports.edit = function (req, res) {
+  res.render ('user/edit', {user: req.user, errors: []});
+};   // req.user: instancia de user cargada con Autoload
+
+//GET /user
+exports.new = function(req, res){
+  var user = models.User.build(// creaobjeto user
+    {username: "", password: ""}
+    );
+    res.render('user/new', {user: user, errors: []});
+};
+
+//POST /user
+exports.create = function(req, res) {
+  var user = models.User.build(req.body.user);
+  user
+  .validate()
+  .then(
+    function(err){
+      if(err) {
+        res.render('user/new', {user: user, errors: err.errors});
+      }
+      else {
+        user //save guarda en DB campos username y password de user
+        .save({fields: ["username", "password"]})
+        .then( function(){
+          //crea la sesion con el usuario autenticado y redirige a /
+          req.session.user = {id:user.id, username:user.username};
+          res.redirect('/');
+        });
+      }
+    }
+  ).catch(function(error){next(error)});
+};
+
+//PUT /user/:id
+exports.update = function (req, res, next){
+  req.user.username = req.body.user.username;
+  req.user.password = req.body.user.password;
+  req.user
+  .validate()
+  .then(
+    function(err){
+      if(err) {
+        res.render('user/' + req.user.id, {user: req.user, errors: err.errors});
+      }
+      else {
+        req.user  //save guardar campo username y pssword en DB
+        .save( {fields: ["Username", "password"]})
+        .then( function(){ res.redirect('/');});
+      } // Redireccion HTTP a /
+    }
+   ).catch(function(error){next(error)});
+};
+
+//DELETE /user/:id
+exports.destroy = function(req, res){
+   req.user.destroy().then(function() {
+   // borra la sesión y redirige a /
+   delete req.session.user;
+   res.redirect('/');
+   }).catch(function(error){next(error)});
 };
--- routes/index.js
+++ routes/index.js
@@ -5,38 +5,51 @@ var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
 var statisticsController = require('../controllers/statistics_controller');
+var userController = require('../controllers/user_controller');
 
-// Página de entrada (home page)
+/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz', errors: []});
+  res.render('index', { title: 'Quiz Edav', errors: [] });
 });
 
-// Autoload de comandos con :quizId
-router.param('quizId', quizController.load);  // autoload :quizId
-router.param('commentId', commentController.load);  // autoload :commentId
+/*
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',   quizController.answer);
+*/
 
-// Definición de rutas de sesion
-router.get('/login',  sessionController.new);     // formulario login
-router.post('/login', sessionController.create);  // crear sesión
-router.get('/logout', sessionController.destroy); // destruir sesión
+// Autoload de comandis con: quizId
+router.param('quizId', quizController.load);   // autoload: quizId
+router.param('commentId', commentController.load);  // autoload :commentId
+router.param('userId', userController.load); //autoload :userId
 
 // Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', 				   sessionController.loginRequired, quizController.new);
-router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
-router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
-router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
+router.get('/quizes',                               quizController.index);
+router.get('/quizes/:quizId(\\d+)',                 quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',          quizController.answer);
+router.get('/quizes/new',                           sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',                       sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',            sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',                 sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',              sessionController.loginRequired, quizController.destroy);
 
-// Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
-router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
-	                                    sessionController.loginRequired, commentController.publish);
+//comentarios
+router.get('/quizes/:quizId(\\d+)/comments/new',    commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',       commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
-// Definición de rutas de estadísticas
-router.get('/quizes/statistics', statisticsController.statistics);
+// Definición de rutas de sesion
+router.get('/login',                                sessionController.new);     // formulario login
+router.post('/login',                               sessionController.create);  // crear sesión
+router.get('/logout',                               sessionController.destroy); // destruir sesión
+
+//definicion de la ruta de estadísticas
+router.get('/quizes/statistics',                    statisticsController.statistics);
+
+//definición de las rutas de cuentas
+router.get('/user',                                 userController.new); //formulario sign up
+router.post('/user',                                userController.create); //registrar usuario
+router.get('/user/:userId(\\d+)/edit',              sessionController.loginRequired, userController.edit);
+router.put('/user/:userId(\\d+)',                   sessionController.loginRequired, userController.update);
+router.delete('/user/:userId(\\d+)',                 sessionController.loginRequired, userController.destroy);
 
 module.exports = router;
